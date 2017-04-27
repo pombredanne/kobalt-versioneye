595719105823ba1cd30fa5aaacd8d9551286b10e
Erik C. Thauvin <erik@thauvin.net>
2017-04-26 20:02:35 -0700

    Added ability to specify version number as an argument.

--- clean.sh
+++ clean.sh
@@ -12,15 +12,21 @@ buildkt="kobalt/src/Build.kt"
 
 name=$(cat $buildkt | grep -m 1 "name = " | cut -d"\"" -f 2)
 group=$(cat $buildkt | grep -m 1 "group = " | cut -d"\"" -f 2)
-version=$(cat $buildkt | grep -m 1 "version = " | cut -d"\"" -f 2)
+
+if [ -z "$1" ]; then
+	version=$(cat $buildkt | grep -m 1 "version = " | cut -d"\"" -f 2)
+else
+	version="$1"
+fi
 
 maven="/k/maven/repository/${group//.//}/${name}/${version}"
 kobalt="$HOME/.kobalt/cache/${group//.//}/${name}/${version}"
+localRepo="$HOME/.kobalt/localMavenRepo/${group//.//}/${name}/${version}"
 
 read -p "Delete version ${version}? " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-	for dir in "$kobalt" "$maven"; do
+	for dir in "$kobalt" "$maven" "$localRepo"; do
 		if [ -d "$dir" ]; then
 			echo -e "Deleting : \e[32;1m$dir\e[0m"
 			$rm "$dir"
